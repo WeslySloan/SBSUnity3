a5acbd8f75c4c4f3d8e5daffa28c8c09f1ed22ed
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2025-01-21 20:40:50 +0900

    (2025.01.21) 카메라 흔들림 효과 구현

--- 2DCombat/Assets/Scripts/Entity/EnemyHealth.cs
+++ 2DCombat/Assets/Scripts/Entity/EnemyHealth.cs
@@ -1,26 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
+using Cinemachine;
 using UnityEngine;
 
+[RequireComponent(typeof(CinemachineImpulseSource))]
 public class EnemyHealth : MonoBehaviour, Idamagable
 {
     // Ã¼·Â
 
     [field: SerializeField] public int CurrentHealth { get; set; }
     [field:SerializeField] public int MaxHealth { get; set; } = 3;
-    public bool HasTakenDamgage { get; set ; } 
+    public bool HasTakenDamgage { get; set ; }
+
+    private CinemachineImpulseSource _impulseSource;
+    [SerializeField] private ScreenShakeSO profile;
 
     private void Start()
     {
         CurrentHealth = MaxHealth;
+        _impulseSource = GetComponent<CinemachineImpulseSource>();
     }
 
     public void Damage(int amount)
     {
         HasTakenDamgage = true;
         CurrentHealth -= amount;
-
+        CameraShakeManager.Instance.CameraShakeFromProfile(_impulseSource, profile);
         Die();
     }
     
