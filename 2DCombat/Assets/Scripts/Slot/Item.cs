6ce08addc7873c4b5be93b6bfa66cfd480367890
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2025-02-06 07:06:09 +0900

    (2025.02.05) 스킬 시스템 구현 및 Swap 시스템

---
+++ 2DCombat/Assets/Scripts/Slot/Item.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "skill", menuName = "Item/skill")]
+public class Iteem : ScriptableObject
+{
+    public string itemName;
+    public Sprite iconImage;
+    // Skill 커스텀 클래스
+
+}
